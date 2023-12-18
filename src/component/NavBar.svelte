bad537dbf8700e0fb016d1f8d1b8a08be65c5b8f
Kevin Osorio <osoriogrisaleskevin@gmail.com>
2023-12-18 17:08:41 -0500

    Update dependencies and fix NavBar styling

--- src/component/NavBar.svelte
+++ src/component/NavBar.svelte
@@ -1,46 +1,46 @@
 <script lang="ts">
-	let selected = $state('/')
-	const routes = {
-		'/': {
-			name: 'Inbox',
-			icon: 'fa-solid fa-inbox'
-		},
-		'/send': {
-			name: 'Send',
-			icon: 'fa-solid fa-paper-plane '
-		},
-		'/trash': {
-			name: 'Trash',
-			icon: 'fa-regular fa-trash-can'
-		},
-		'/profile': {
-			name: 'Profile',
-			icon: 'fa-solid fa-user'
-		}
-	}
+  let selected = $state('/')
+  const routes = {
+    '/': {
+      name: 'Inbox',
+      icon: 'fa-solid fa-inbox'
+    },
+    '/send': {
+      name: 'Send',
+      icon: 'fa-solid fa-paper-plane '
+    },
+    '/trash': {
+      name: 'Trash',
+      icon: 'fa-regular fa-trash-can'
+    },
+    '/profile': {
+      name: 'Profile',
+      icon: 'fa-solid fa-user'
+    }
+  };
 </script>
 
 <nav class="bg-[#1C1B1B] py-3 h-full lg:p-4">
-	<ul class="flex justify-evenly lg:flex-col lg:space-y-5">
-		{#each Object.entries(routes) as [route, value]}
-			<li>
-				<a
-					class={`${selected === route ? 'selected' : undefined}`}
-					href={route}
-					onclick={() => selected = route}
-				>
-					<i class={value.icon} /></a
-				>
-			</li>
-		{/each}
-	</ul>
+  <ul class="flex justify-evenly lg:flex-col lg:space-y-5">
+    {#each Object.entries(routes) as [route, value]}
+    <li>
+        <a
+          class={`${selected === route ? 'selected' : undefined}`}
+          href={route}
+          onclick={() => (selected = route)}
+          >
+          <i class={value.icon} /></a
+        >
+      </li>
+    {/each}
+  </ul>
 </nav>
 
 <style scoped>
-li a{
-	@apply px-4 py-2 rounded-lg hover:bg-[#242434]
-}
-.selected{
-	@apply bg-[#12CCE1] hover:bg-[#12CCE1]
-}
+  li a {
+    @apply rounded-lg px-4 py-2 hover:bg-[#242434];
+  }
+  .selected {
+    @apply bg-[#12CCE1] hover:bg-[#12CCE1];
+  }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script>
-	import "../app.pcss";
-    import NavBar from '../component/NavBar.svelte';
-    import './styles.css';
+	import '../app.pcss';
+	import NavBar from '../component/NavBar.svelte';
+	import './styles.css';
 </script>
 
 <svelte:head>
 	<script src="https://kit.fontawesome.com/f37da43382.js" crossorigin="anonymous"></script>
 </svelte:head>
 
-<div class="app flex flex-col h-screen lg:flex-row-reverse">
-	<main class="flex-grow bg-[#242434] p-10">
+<div class="app flex flex-col h-screen max-h-screen overflow-hidden lg:flex-row-reverse">
+	<main class="flex-grow overflow-y-auto bg-[#242434] p-10">
 		<slot />
 	</main>
 	<div class="sticky bottom-0 lg:left-0">
